9c6efcc40bfcb27b90be9b5ee7c6936232239a31
Steve Dunstan <steve_dunstan@yahoo.com>
2016-08-07 22:23:50 -0700

    Removed audio html element in favor of AudioContext.
    Sets up for future beatmatching and fading.

---
+++ audio.js
@@ -0,0 +1,59 @@
+// TODO: refactor to pure functions. Take all this state out of here.
+var audio = (function() {
+  var deckA, deckB, playingA = false, context, bufferA, offsetA;
+  return {
+    isPlayingA: function() { return playingA; },
+    loadA: function(fileName, deckConnected) {
+      if (playingA) {
+        audio.pauseA();
+      }
+      deckA = null;
+      if (context) {
+        context.close();
+        context = null;
+        bufferA = null;
+      }
+      offsetA = 0.0;
+      context = new AudioContext();
+      fs.readFile(fileName, (err, fileData) => {
+        context.decodeAudioData(fileData.buffer, (audioBuffer) => {
+          bufferA = audioBuffer;
+          var deck = context.createBufferSource();
+          deck.buffer = bufferA;
+          deck.connect(context.destination);
+          deckA = deck;
+          deckConnected && deckConnected();
+        });
+      });
+    },
+    playA: function() {
+      if (deckA && !playingA) {
+        playingA = true;
+        deckA.start(0, offsetA);
+      }
+    },
+    pauseA: function() {
+      if (deckA || playingA) {
+        offsetA = context.currentTime;
+        playingA = false;
+        deckA.stop();
+        deckA = null;
+
+        var deck = context.createBufferSource();
+        deck.buffer = bufferA;
+        deck.connect(context.destination);
+        deckA = deck;
+      }
+    },
+    toggleA: function() {
+      if (deckA) {
+        if (playingA) {
+          audio.pauseA();
+        }
+        else {
+          audio.playA();
+        }
+      }
+    }
+  }
+}());
--- renderer.js
+++ renderer.js
@@ -1,33 +1,16 @@
 var jsmediatags = require('jsmediatags');
-var audioElement = document.getElementById("deck"),
-  scrubber = document.getElementById("scrubber"),
+var scrubber = document.getElementById("scrubber"),
   songListElement = document.getElementById("songListElement");
 
-var play = function() {
-  audioElement.play();
-  playButton.classList.remove("fa-play");
-  playButton.classList.add("fa-pause");
-};
-
-var pause = function() {
-  audioElement.pause();
-  playButton.classList.remove("fa-pause");
-  playButton.classList.add("fa-play");
-};
-
-var playSong = function(path) {
-  setupSong({path:path});
-}
-
 var setupSong = function(song) {
-  pause();
   scrubber.max = 100;
   scrubber.value = 0;
+  playButton.classList.add("fa-play");
+  playButton.classList.remove("fa-pause");
 
-  deck.src = "playlist/" + song.path;
-  // play();
+  audio.loadA("playlist/" + song);
 
-  jsmediatags.read("playlist/" + song.path, {
+  jsmediatags.read("playlist/" + song, {
     onSuccess: function(tag) {
       // console.log(tag);
       document.getElementById("title").innerText = tag.tags.title;
@@ -46,27 +29,21 @@ document.getElementById("playButton").onclick =
   function() {
     var playButton = document.getElementById("playButton");
 
-    if (audioElement.paused === true) {
-      play();
-    }
-    else {
-      pause();
-    }
-    scrubber.max = audioElement.duration;
-    scrubber.value = audioElement.currentTime;
-  };
+    audio.toggleA();
+    playButton.classList.toggle("fa-play");
+    playButton.classList.toggle("fa-pause");
 
-audioElement.addEventListener('timeupdate', function() {
-  scrubber.value = audioElement.currentTime;
-});
+    // scrubber.max = audioElement.duration;
+    // scrubber.value = audioElement.currentTime;
+  };
 
 playlist.parsePlaylist(
     function(songList) {
-      setupSong(songList[0]);
+      setupSong(songList[0].path);
       songList.forEach(function(song, index) {
         jsmediatags.read("playlist/" + song.path, {
           onSuccess: function(tag) {
-            songListElement.insertAdjacentHTML('beforeend', `<li onclick='playSong("${song.path}")' id='#song${index}'><b>${tag.tags.title}</b><br>${tag.tags.artist}</li>`);
+            songListElement.insertAdjacentHTML('beforeend', `<li onclick='setupSong("${song.path}")' id='#song${index}'><b>${tag.tags.title}</b><br>${tag.tags.artist}</li>`);
           },
           onError: function(error) {
             console.log(error);
